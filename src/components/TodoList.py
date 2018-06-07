7cb246639ebb7ce0474ff920bff0e6676337d268
Marc Wood <marcdw87@gmail.com>
2018-06-07 13:48:54 -0500

    Use fontawesome check icon to mark completed todos.

--- src/components/TodoList.py
+++ src/components/TodoList.py
@@ -5,6 +5,7 @@ React = require("react")
 ListGroup, ListGroupItem, Button = destruct(
     require("reactstrap"), "ListGroup", "ListGroupItem", "Button")
 RingLoader = require("react-spinners").RingLoader
+FontAwesomeIcon = require("react-fontawesome")
 
 
 class TodoList(Component):
@@ -17,40 +18,55 @@ class TodoList(Component):
     def on_click_delete(self, todo_id):
         return lambda: console.log(todo_id)
 
-    def render(self):
-        def render_list_item(todo):
-            return __pragma__("xtrans", None, "{}", """ (
-                <ListGroupItem
-                    className="flex-center"
-                    key={todo.id}
-                >
-                    <div className="list-item">
-                        {todo.text}
-                    </div>
-                    <Button
-                        color="primary"
-                        onClick={self.on_click_complete(todo.id)}
-                    >Complete</Button>
-                    <Button
-                        color="danger"
-                        onClick={self.on_click_delete(todo.id)}
-                    >Delete</Button>
-                </ListGroupItem>
-            ); """)
+    def render_spinner(self):
+        loading = True
+        return __pragma__("xtrans", None, "{}", """ (
+            <div className="flex-center">
+                <RingLoader
+                    color="#999"
+                    loading={loading}
+                />
+            </div>
+        ); """)
 
-        todo_list = self.props["todo_list"]
-        if todo_list["loading"]:
-            loading = True
+    def render_checkmark(self, todo):
+        if todo["complete"]:
             return __pragma__("xtrans", None, "{}", """ (
-                <div className="flex-center">
-                    <RingLoader
-                        color={"#999"}
-                        loading={loading}
-                    />
+                <div className="flex-column flex-center margin">
+                    <FontAwesomeIcon
+                        name="check-circle"
+                        size="2x"
+                        className="green-text" />
                 </div>
             ); """)
 
-        list_items = map(render_list_item, todo_list["todos"])
+    def render_list_item(self, todo):
+        return __pragma__("xtrans", None, "{}", """ (
+            <ListGroupItem
+                className="flex-center"
+                key={todo.id}
+            >
+                {self.render_checkmark(todo)}
+                <div className="list-item">
+                    {todo.text}
+                </div>
+                <Button
+                    color="primary"
+                    onClick={self.on_click_complete(todo.id)}
+                >Complete</Button>
+                <Button
+                    color="danger"
+                    onClick={self.on_click_delete(todo.id)}
+                >Delete</Button>
+            </ListGroupItem>
+        ); """)
+
+    def render(self):
+        todo_list = self.props["todo_list"]
+        if todo_list["loading"]:
+            return self.render_spinner()
+
+        list_items = map(self.render_list_item, todo_list["todos"])
         return __pragma__("xtrans", None, "{}", """ (
             <div>
                 <ListGroup>
