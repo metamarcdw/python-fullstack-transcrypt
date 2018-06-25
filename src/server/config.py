7a4810392db6684d4866b64b9aa9e586e77ed3a0
Marc Wood <marcdw87@gmail.com>
2018-06-24 23:42:09 -0500

    Read production secret from file at import time.

--- src/server/config.py
+++ src/server/config.py
@@ -1,5 +1,15 @@
 import os
 
+path = os.path.join(os.path.dirname(__file__), "prod_secret.txt")
+try:
+    with open(path, "r") as file_:
+        prod_secret = file_.read().strip()
+except FileNotFoundError as e:
+    raise FileNotFoundError("Production secret file does not exist.") from e
+if not prod_secret:
+    raise ValueError("Production secret file is blank.")
+
+
 class Config:
     DEBUG = False
     TESTING = False
@@ -7,24 +17,18 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     JWT_SECRET_KEY = "asecret"
 
+
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = \
         "sqlite:////home/metamarcdw/python-fullstack-transcrypt/src/server/db.sqlite3"
+    JWT_SECRET_KEY = prod_secret
 
-    def __init__(self):
-        path = os.path.join(os.path.dirname(__file__), "prod_secret.txt")
-        try:
-            with open(path, "r") as file_:
-                self.JWT_SECRET_KEY = file_.read().strip()
-        except FileNotFoundError as e:
-            raise FileNotFoundError("Production secret file does not exist.") from e
-        if not self.JWT_SECRET_KEY:
-            raise ValueError("Production secret file is blank.")
 
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = \
         r"sqlite:///C:\Users\cypher\Desktop\fullstack-react\todos_fs\src\server\db.sqlite3"
+
 
 class TestingConfig(Config):
     TESTING = True
