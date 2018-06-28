c71a557fc8cc6109fb3e7b3bae0ab4511318409b
Marc Wood <marcdw87@gmail.com>
2018-06-28 16:51:50 -0500

    Add test for successful return of all users.

--- src/tests/test_user_endpoints.py
+++ src/tests/test_user_endpoints.py
@@ -1,5 +1,6 @@
 import pytest
 from flask import url_for
+from server.backend import UserUtil
 
 
 def test_get_all_users_unauthorized(client):
@@ -14,3 +15,15 @@ def test_get_all_users_forbidden(client, user_token):
     })
     assert res.status_code == 403
     assert res.json == {"message": "Must be admin"}
+
+
+def test_get_all_users_success(client, admin_token):
+    res = client.get(url_for("users_resource"), headers={
+        "Authorization": f"Bearer {admin_token}"
+    })
+    assert res.status_code == 200
+    assert "users" in res.json
+    users = res.json["users"]
+    assert users
+    for user in users:
+        UserUtil.user_shape.validate(user)
