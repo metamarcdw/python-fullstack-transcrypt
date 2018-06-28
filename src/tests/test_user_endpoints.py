4443cb9aea0fa98495e2720fa877875f90288644
Marc Wood <marcdw87@gmail.com>
2018-06-28 16:19:29 -0500

    Add assertions for JSON to current tests.

--- src/tests/test_user_endpoints.py
+++ src/tests/test_user_endpoints.py
@@ -5,6 +5,7 @@ from flask import url_for
 def test_get_all_users_unauthorized(client):
     res = client.get(url_for("users_resource"))
     assert res.status_code == 401
+    assert res.json == {"msg": "Missing Authorization Header"}
 
 
 def test_get_all_users_forbidden(client, user_token):
@@ -12,3 +13,4 @@ def test_get_all_users_forbidden(client, user_token):
         "Authorization": f"Bearer {user_token}"
     })
     assert res.status_code == 403
+    assert res.json == {"message": "Must be admin"}
