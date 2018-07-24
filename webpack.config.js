ce2e3fd86cdf42eef3013f1161b63db1efcf7560
Marc Wood <marcdw87@gmail.com>
2018-07-24 04:32:26 -0500

    Cleanup useless comments in webpack.config.js.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,16 @@
-const webpack = require('webpack');
-const path = require('path');
-
-/*
- * We've enabled UglifyJSPlugin for you! This minifies your app
- * in order to load faster and run less javascript.
- *
- * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
- *
- */
-
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
   mode: "development",
   stats: { performance: false },
 
-  entry: './src/__javascript__/index.js',
+  entry: "./src/__javascript__/index.js",
 
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
 
   module: {
@@ -31,14 +22,6 @@ module.exports = {
     compress: true,
     host: "localhost",
     port: 3000
-    // proxy: {
-    //   "/api": {
-    //     target: 'http://metamarcdw.pythonanywhere.com',
-    //     pathRewrite: { '^/api': '' },
-    //     changeOrigin: true,
-    //     secure: false
-    //   }
-    // }
   },
 
   plugins: [new UglifyJSPlugin()]
